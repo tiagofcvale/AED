7545281e07552d94f27f007036f07546d21dd59c
Tiago <tiagofcvale@ua.pt>
2025-09-16 12:47:50 +0100

    .

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World\n");
+
+    int a = 1;
+    int b = 5;
+    int c = a + b;
+
+    printf("The sum is: %d\n", c);
+    printf("\n nigga");
+
+    return 0;
+}
