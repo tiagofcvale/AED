53365c9346c506469daea225f9bb726b559bb1a4
tiagofcvale <tiagofcvale@ua.pt>
2025-10-23 12:23:41 +0100

    delannoy_memoization

--- Praticas/AED_Guiao_06/delannoy.c
+++ Praticas/AED_Guiao_06/delannoy.c
@@ -5,13 +5,8 @@
 long delannoy(int, int);
 unsigned long ADDS;
 unsigned long delannoy_dinamic(int, int);
-long int delCache[SIZE];
-
-void initCache(void) {
-    for (size_t i = 0; i < SIZE; i++) {
-        delCache[i] = -1;
-    }
-}
+long int delCache[SIZE][SIZE];
+unsigned long delannoy_memoization(int, int);
 
 int main(int argc, char *argv[]) { 
 
@@ -23,11 +18,21 @@ int main(int argc, char *argv[]) {
     printf("ADDS: %ld\n\n",ADDS);
     ADDS = 0;
 
-    unsigned long resultado = delannoy_dinamic(m, n);
-    printf("D(%d, %d) = %lu\n", m, n, resultado);
-    printf("ADDS: %ld\n",ADDS);
+    unsigned long resd = delannoy_dinamic(m, n);
+    printf("D(%d, %d) = %lu\n", m, n, resd);
+    printf("ADDS: %ld\n\n",ADDS);
     ADDS=0;
 
+    // INICIALIZAR delCache 
+    for (int i = 0; i < SIZE; i++){
+        for (int j = 0; j < SIZE; j++){
+            delCache[i][j] = -1;
+        }
+    }
+
+    unsigned long resm = delannoy_memoization(m, n);
+    printf("D(%d, %d) = %lu\n", m, n, resm);
+    printf("ADDS: %ld\n",ADDS);
     return 0;
 }
 
@@ -59,6 +64,23 @@ unsigned long delannoy_dinamic(int m, int n) {
     }
 
     return D[m][n];
+}
+
+unsigned long delannoy_memoization(int m, int n) {
+    if (delCache[m][n] != -1) return delCache[m][n];
+
+    if (m == 0 || n == 0) {
+        return delCache[m][n] = 1;
+    }
+
+
+    ADDS+=2;
+    delCache[m][n] =
+        delannoy_memoization(m - 1, n)
+        + delannoy_memoization(m - 1, n - 1)
+        + delannoy_memoization(m, n - 1);
+
+    return (delCache[m][n]);
 }
 
 // O MAIOR NÚMERO QUE CONSIGO DETERMINAR É 14
