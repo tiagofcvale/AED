c113030b4b77ba67cb1dbd76ca6481d6161ddb1f
tiagofcvale <tiagofcvale@ua.pt>
2025-10-30 12:06:48 +0000

    MAKE FILES É BUE TOPPOPOPOPOPOP

--- Praticas/AED_Guiao_07/Guiao_07_Ficheiros_para_os_Estudantes/01_STACK_and_QUEUE_example/01_example.c
+++ Praticas/AED_Guiao_07/Guiao_07_Ficheiros_para_os_Estudantes/01_STACK_and_QUEUE_example/01_example.c
@@ -1,6 +1,6 @@
 //
 // TO DO : desenvolva um algoritmo para verificar se um numero inteiro positivo
-//         e uma capicua
+//         é uma capicua
 //         Exemplos: 12321 e uma capiacua, mas 123456 nao e
 //         USE uma PILHA DE INTEIROS (STACK) e uma FILA DE INTEIROS (QUEUE)
 //
@@ -11,8 +11,53 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "IntegersQueue.h"
 #include "IntegersStack.h"
 
-int main(void) { return 0; }
+int isPalindrome(int number) {
+    Queue* queue = QueueCreate(100); // maxSize = 100
+    Stack* stack = StackCreate(100);
+
+    // Extract numbers
+    while (number > 0) {
+        int digit = number % 10;
+
+        QueueEnqueue(queue, digit);
+        StackPush(stack, digit);
+        
+        number /= 10;
+    }
+
+    // Compare elements of stack and queue
+    while(!QueueIsEmpty(queue) || !StackIsEmpty(stack)) {
+        if (QueueDequeue(queue) != StackPop(stack)) {
+
+            QueueDestroy(&queue);
+            StackDestroy(&stack);
+            return 0; // Not palindrome 
+        }
+    }
+
+    QueueDestroy(&queue);
+    StackDestroy(&stack);
+    return 1; // Palindrome
+}
+int main(int argc, char* argv[]) { 
+    int number;
+    
+    if (argc != 2) {
+        printf("Usage: %s <number>\n",argv[0]);
+        return 1;
+    }
+
+    number = atoi(argv[1]);
+
+    if (isPalindrome(number)) {
+        printf("%d é capicua\n",number);
+    } else {
+        printf("%d não é capicua\n",number);
+    }
+    return 0;
+}
