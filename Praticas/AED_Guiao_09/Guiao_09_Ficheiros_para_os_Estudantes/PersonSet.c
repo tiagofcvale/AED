7e924037bdf1464730fb1775fb14fb72eb0db734
tiagofcvale <tiagofcvale@ua.pt>
2025-11-13 12:14:29 +0000

    ps to pps

--- Praticas/AED_Guiao_09/Guiao_09_Ficheiros_para_os_Estudantes/PersonSet.c
+++ Praticas/AED_Guiao_09/Guiao_09_Ficheiros_para_os_Estudantes/PersonSet.c
@@ -39,10 +39,10 @@ PersonSet *PersonSetCreate() {
   // You must allocate space for the struct and create an empty persons list!
   // COMPLETE ...
 
-  PersonSet *ps = (PersonSet *)malloc(sizeof(PersonSet));
-  if (ps == NULL) return NULL;
-  ps->persons = ListCreate(cmpP);
-  return ps;
+  PersonSet *pps = (PersonSet *)malloc(sizeof(PersonSet));
+  if (pps == NULL) return NULL;
+  pps->persons = ListCreate(cmpP);
+  return pps;
 }
 
 // Destroy PersonSet *pps
