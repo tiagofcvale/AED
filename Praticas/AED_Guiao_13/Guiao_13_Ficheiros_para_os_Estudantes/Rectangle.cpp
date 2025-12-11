1a03cc6c29293fc68b643bc863ee3396ac8d66e4
tiagofcvale <tiagofcvale@ua.pt>
2025-12-11 12:51:01 +0000

    Fim aula

--- Praticas/AED_Guiao_13/Guiao_13_Ficheiros_para_os_Estudantes/Rectangle.cpp
+++ Praticas/AED_Guiao_13/Guiao_13_Ficheiros_para_os_Estudantes/Rectangle.cpp
@@ -14,49 +14,64 @@
 
 #include "Point.h"
 
-Rectangle::Rectangle(void) {
-  // Rectangle of width=1 and height=1 and centered at (0,0)
-  // COMPLETE
+Rectangle::Rectangle(void) 
+  : width_(1.0), height_(1.0), Figure(Point(0.0,0.0),"black"){
 }
 
 Rectangle::Rectangle(Point center, const std::string& color, double width,
-                     double height) {
-  // Ensure that the width and height are larger than 0.0
-  // COMPLETE
+                     double height)
+                      : Figure(center,color), height_(height > 0.0 ? height : 1.0), width_(width > 0.0 ? width : 1.0) {
 }
 
 Rectangle::Rectangle(double x, double y, const std::string& color, double width,
-                     double height) {
-  // Ensure that the width and height are larger than 0.0
-  // COMPLETE
+                     double height) 
+                      : Figure(Point(x,y),color), height_(height > 0.0 ? height : 1.0), width_(width > 0.0 ? width : 1.0) {
 }
 
 double Rectangle::GetHeight(void) const { return height_; }
 void Rectangle::SetHeight(double length) {
-  // Ensure that the height is larger than 0.0
-  // COMPLETE
+  height_ = (length > 0.0 ? length : 1.0);
 }
 
 double Rectangle::GetWidth(void) const { return width_; }
 void Rectangle::SetWidth(double length) {
-  // Ensure that the width is larger than 0.0
-  // COMPLETE
+  width_ = (length > 0.0 ? length : 1.0);
 }
 
 std::string Rectangle::GetClassName(void) const { return "Rectangle"; }
 
 double Rectangle::Area(void) const {
-  // COMPLETE
+  return width_ * height_;
 }
 
 double Rectangle::Perimeter(void) const {
-  // COMPLETE
+  return width_ * 2 + height_ * 2; 
 }
 
 std::ostream& operator<<(std::ostream& os, const Rectangle& obj) {
-  // COMPLETE
+    os << obj.GetCenter().GetX() << " "
+       << obj.GetCenter().GetY() << " "
+       << obj.GetColor() << " "
+       << obj.width_ << " "
+       << obj.height_;
+
+    return os;
 }
 
 std::istream& operator>>(std::istream& is, Rectangle& obj) {
-  // COMPLETE
+    double x, y, w, h;
+    std::string color;
+
+    // Ler valores pela mesma ordem em que os escrevemos
+    is >> x >> y >> color >> w >> h;
+
+    // Atualizar o objeto
+    obj.SetCenter(Point(x, y));
+    obj.SetColor(color);
+
+    // Garantir dimensões válidas
+    obj.width_  = (w > 0.0 ? w : 1.0);
+    obj.height_ = (h > 0.0 ? h : 1.0);
+
+    return is;
 }
