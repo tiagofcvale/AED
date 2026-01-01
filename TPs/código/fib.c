8ea9aa42ba6282c8e2c39b364bd4320ee353832b
tiagofcvale <tiagofcvale@ua.pt>
2026-01-01 22:48:21 +0000

    fib

--- TPs/código/fib.c
+++ TPs/código/fib.c
@@ -1,5 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define SIZE 100
+
+long int Fib_Cache[SIZE];
+
+void initCache(void) {
+    for (size_t i = 0; i < SIZE; i++) {
+        Fib_Cache[i] = -1;
+    }
+}
 
 int fibRec(int n) {
     if (n == 0) return 0;
@@ -20,12 +29,27 @@ int fibDin(int n) {
     return fib[n];
 }
 
+int fibMem(int n) {
+    if(Fib_Cache[n] != -1) return Fib_Cache[n];
+    long int r;
+    if(n==0) r = 0;
+    else if(n==1) r = 1;
+    else r = fibMem(n-2) + fibMem(n-1);
+
+    Fib_Cache[n] = r;
+    return r;
+}
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         printf("Uso: %s <numero>\n",argv[0]);
         return -1;
     }
+
+    initCache();
+
     // printf("%d\n",fibRec(atoi(argv[1])));
-    printf("%d\n",fibDin(atoi(argv[1])));
+    // printf("%d\n",fibDin(atoi(argv[1])));
+    printf("%d\n",fibMem(atoi(argv[1])));
     return 0;
 }
